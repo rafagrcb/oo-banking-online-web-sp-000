f6dc052c2abee88254802e055a33c4bce71c6ea7
Rafael Garcia <rafagrcb@gmail.com>
2020-09-29 16:06:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
 def execute_transaction
     if @sender.balance < @amount
       puts "Transaction rejected. Please check your account balance."
-    elsif @status == "complete"
+    elsif @status == "rejected"
       return "Transaction was already excuted"
     else
       @sender.deposit( @amount * -1 ) 
